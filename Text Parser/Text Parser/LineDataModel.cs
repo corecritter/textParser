48ff2b7a23774fb48e8210b8dccda012f6119530
corecritter <bobbysavel@gmail.com>
2022-02-02 20:07:55 -0500

    Initial commit

---
+++ Text Parser/Text Parser/LineDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Parser
+{
+    public class LineDataModel
+    {
+        public DateTime Timestamp { get; set; }
+        public string Sender { get; set; }
+        public string Language { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
